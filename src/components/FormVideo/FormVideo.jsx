e945ff73bc5f8ea76f9c829547063df77817ace2
Ulises <ulises493rojas@gmail.com>
2023-10-03 23:03:21 -0600

    Se dio estilización a las botones del componente FormVideo

--- src/components/FormVideo/FormVideo.jsx
+++ src/components/FormVideo/FormVideo.jsx
@@ -5,6 +5,7 @@ import { Form, TitleMedium, } from '../UI/Styles';
 import DefaultPage from '../DefaultPage';
 import styled from 'styled-components';
 import {
+    colorBlackDark,
     colorBlackLight,
     colorBlackLighter,
     colorBlackMedium,
@@ -16,6 +17,18 @@ import Button from '../Button';
 const TitleContainer = styled.div`
     margin: 3rem 0;
     text-align: center;
+`;
+
+const ContainerButtons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 1.5rem 0;
+`;
+
+const ContainerSubmit = styled.div`
+    display: flex;
+    gap: 1rem;
 `;
 
 const customInputStyles = {
@@ -56,59 +69,64 @@ const FormVideo = () => {
                 <TitleMedium>Nuevo Video</TitleMedium>
             </TitleContainer>
 
-            
-                <Form onSubmit={(e) => {
-                    e.preventDefault();
-                }}
+            <Form onSubmit={(e) => {
+                e.preventDefault();
+            }}
+            >
+                <TextField
+                    id="filled-basic"
+                    label="Titulo"
+                    variant="filled"
+                    margin="normal"
+                    sx={customInputStyles}
+                />
+                <TextField
+                    id="filled-basic"
+                    label="Link del video"
+                    variant="filled"
+                    margin="normal"
+                    sx={customInputStyles}
+                />
+                <TextField
+                    id="filled-basic"
+                    label="Link de imagen del video"
+                    variant="filled"
+                    margin="normal"
+                    sx={customInputStyles}
+                />
+
+                <TextField
+                    id="filled-select-currency"
+                    select
+                    label="Categoría"
+                    defaultValue="EUR"
+                    variant="filled"
+                    margin="normal"
+                    value={category}
+                    onChange={handleChange}
+                    sx={customInputStyles}
                 >
-                    <TextField
-                        id="filled-basic"
-                        label="Titulo"
-                        variant="filled"
-                        margin="normal"
-                        sx={customInputStyles}
-                    />
-                    <TextField
-                        id="filled-basic"
-                        label="Link del video"
-                        variant="filled"
-                        margin="normal"
-                        sx={customInputStyles}
-                    />
-                    <TextField
-                        id="filled-basic"
-                        label="Link de imagen del video"
-                        variant="filled"
-                        margin="normal"
-                        sx={customInputStyles}
-                    />
+                    <MenuItem value={10}>1</MenuItem>
+                </TextField>
 
-                    <TextField
-                        id="filled-select-currency"
-                        select
-                        label="Categoría"
-                        defaultValue="EUR"
-                        variant="filled"
-                        margin="normal"
-                        value={category}
-                        onChange={handleChange}
-                        sx={customInputStyles}
-                    >
-                        <MenuItem value={10}>1</MenuItem>
-                    </TextField>
-
-                    <TextField
-                        id="filled-basic"
-                        label="Descripción"
-                        variant="filled"
-                        margin="normal"
-                        sx={customInputStyles}
-                    />
-
-                    <Button type='sumbit'>Guardar</Button>
-                    <Button >Limpiar</Button>
+                <TextField
+                    id="filled-basic"
+                    label="Descripción"
+                    variant="filled"
+                    margin="normal"
+                    sx={customInputStyles}
+                />
+                <ContainerButtons>
+                    <ContainerSubmit>
+                        <Button type='sumbit'>Guardar</Button>
+                        <Button
+                            color={colorBlackDark}
+                            backgroundColor={colorBlackLighter}
+                        >Limpiar</Button>
+                    </ContainerSubmit>
                     <Button >Nueva Categoría</Button>
-                </Form>
+                </ContainerButtons>
+            </Form>
 
         </DefaultPage>
     )
